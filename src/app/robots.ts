21917ed90d781d8ac42dfa08980ed1bc5c75996e
ArunaAP <it20657796@gmail.com>
2026-06-25 18:54:46 +0530

    add robots.ts file

---
+++ src/app/robots.ts
@@ -0,0 +1,11 @@
+import { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap: "https://theiqonex.com/sitemap.xml",
+  };
+}
